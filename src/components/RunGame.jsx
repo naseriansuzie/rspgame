09425da1f8ad643456b3b281cefb7a99cd67babb
Sooji Kim <yh0gogo@gmail.com>
2020-03-25 12:17:33 +0900

    Create component initially

---
+++ src/components/RunGame.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
+
+@observer
+class RunGame extends Component {
+  render() {
+    return <div>run game</div>;
+  }
+}
+export default RunGame;
---
+++ src/components/TotalScore.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { observer, inject } from "mobx-react";
+
+@observer
+class TotalScore extends Component {
+  render() {
+    return <div>score</div>;
+  }
+}
+export default TotalScore;
