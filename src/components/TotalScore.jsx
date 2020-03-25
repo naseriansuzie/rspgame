23340426e9bdc4d9afe743e29ce6b673521eb09b
Sooji Kim <yh0gogo@gmail.com>
2020-03-25 22:24:00 +0900

    Move round status from GameBoard to TotalScore

--- src/components/TotalScore.jsx
+++ src/components/TotalScore.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import { observer, inject } from "mobx-react";
 
+@inject("game", "setup")
 @observer
 class TotalScore extends Component {
   render() {
-    return <div>score</div>;
+    const { round } = this.props.game;
+    const { gameSet, currentSet } = this.props.setup;
+    return (
+      <div>
+        <ul>
+          <li>Total Set : {gameSet}</li>
+          <li>
+            NOW : {currentSet}세트 {round} 번째 판
+          </li>
+        </ul>
+        <p></p>
+      </div>
+    );
   }
 }
 export default TotalScore;
