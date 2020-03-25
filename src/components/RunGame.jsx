e991bce98427f15080af23673b8714b6b05642d4
Sooji Kim <yh0gogo@gmail.com>
2020-03-25 15:13:42 +0900

    Set basic rsp game mode and apply proper functions
    - apply setComputerHand, runGame function
    - add classNames and css

--- src/components/RunGame.jsx
+++ src/components/RunGame.jsx
@@ -1,10 +1,49 @@
 import React, { Component } from "react";
 import { observer, inject } from "mobx-react";
+import "./rungame.css";
 
+@inject("game")
 @observer
 class RunGame extends Component {
   render() {
-    return <div>run game</div>;
+    const {
+      currentGameSet,
+      setComputerHand,
+      computerHand,
+      result,
+    } = this.props.game;
+    return (
+      <div className="game-board">
+        <div>
+          <p>{currentGameSet}세트</p>
+        </div>
+        <div className="hand-container">
+          <div className="hands">
+            <p className="description">패를 선택해주세요.</p>
+            <div className="rsp-container">
+              <button className="rsp" onClick={() => setComputerHand(1)}>
+                가위
+              </button>
+              <button className="rsp" onClick={() => setComputerHand(0)}>
+                바위
+              </button>
+              <button className="rsp" onClick={() => setComputerHand(-1)}>
+                보
+              </button>
+            </div>
+          </div>
+          <div className="hands">
+            <p className="description">상대방의 패입니다.</p>
+            <p>{computerHand}</p>
+          </div>
+        </div>
+        {result ? (
+          <div className="score-result">이번 판 결과 - {result}</div>
+        ) : (
+          <div />
+        )}
+      </div>
+    );
   }
 }
 export default RunGame;
