0a0072355c046d22932e48c04d6482a75d290d40
Sooji Kim <yh0gogo@gmail.com>
2020-03-25 12:16:45 +0900

    Change header text name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <div className="App">RSP Game</div>
+      <div className="App">
+        <h1>가위바위보 게임</h1>
+      </div>
       <Switch>
         <Route exact path="/" component={Intro} />
         <Route path="/rsp" component={Game} />
