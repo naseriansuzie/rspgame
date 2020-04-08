71df50cea9130556d1c0f93f37d3bfabd10d46a2
angie <angie@watcha.com>
2020-04-08 18:59:21 +0900

    Create constant variables separately

---
+++ src/constant.js
@@ -0,0 +1,10 @@
+export const ROCK = '바위';
+export const SCISSORS = '가위';
+export const PAPER = '보';
+export const AUTOWIN = "자동 승리";
+
+export const COMPUTER = '컴퓨터';
+export const PLAYER = '플레이어';
+export const DRAW = '무승부';
+
+export const RSP = {[ROCK] : 0, [SCISSORS] : 1, [PAPER] : -1}
