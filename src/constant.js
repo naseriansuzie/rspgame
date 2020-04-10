f71cbb2f914b377fe177b3412b53fdeaf6427502
angie <angie@watcha.com>
2020-04-10 14:00:22 +0900

    Flip rock, scissors, paper order

--- src/constant.js
+++ src/constant.js
@@ -6,7 +6,7 @@ export const AUTOWIN = "auto win";
 export const COMPUTER = "computer";
 export const PLAYER = "player";
 
-export const RSP = { [ROCK]: 0, [SCISSORS]: 1, [PAPER]: -1 };
+export const RSP = { [SCISSORS]: 1, [ROCK]: 0, [PAPER]: -1 };
 
 export const MIN_GAME_SET = 1;
 export const MIDDLE_ROUND = 2;
